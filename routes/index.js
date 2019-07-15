0e1980e9042e38edab18d8962dd31e05891794a1
tlp <thierrylepors@gmail.com>
2019-07-15 14:36:10 +0200

    test sans mysql

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var con = mysql.createConnection({
+/*var con = mysql.createConnection({
   host: "btynqm6xaoikm4l8eix4-mysql.services.clever-cloud.com",
   user: "uulhz2pqtzq43cz5",
   password: "TtttrS8HsgbIROoJblTM",
   database: "btynqm6xaoikm4l8eix4"
-});
+});*/
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  con.connect(function(err) {
+  /*con.connect(function(err) {
     if (err) throw err;
     con.query("SELECT * FROM employe where num_service = 30", function (err, result, fields) {
       if (err) throw err;
     });
-  });
+  });*/
 
   res.render('index', { title: 'TpMYSQL' });
 });
 
 
 router.get('/salarie', function(req,res,next){
-  con.connect(function(err) {
+  /*con.connect(function(err) {
     if (err) throw err;
     con.query("SELECT * FROM employe where num_service = 20", function (err, result, fields) {
       if (err) throw err;
     });
     res.render('salarie', { title: 'TpMYSQL' });
-})});
+})*/
+});
 
 module.exports = router
