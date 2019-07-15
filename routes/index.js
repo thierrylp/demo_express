b121c9236637b69945d11af52c22344717d27769
tlp <thierrylepors@gmail.com>
2019-07-15 15:12:40 +0200

    test sql 4

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,65 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "btynqm6xaoikm4l8eix4-mysql.services.clever-cloud.com",
-  user: "uulhz2pqtzq43cz5",
-  password: "TtttrS8HsgbIROoJblTM",
-  database: "btynqm6xaoikm4l8eix4"
-});
+var dbConfig = {
+  host     : process.env.MYSQL_ADDON_HOST,
+  database : process.env.MYSQL_ADDON_DB,
+  user     : process.env.MYSQL_ADDON_USER,
+  password : process.env.MYSQL_ADDON_PASSWORD
+};
+
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect( function onConnect(err) {   // The server is either down
+        if (err) {                                  // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
+        console.log('db error', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+handleDisconnect();
+
 con.connect(function(err) {
-  if (err) throw err;
+  if (err) 
+  {
+    console.log("!!!!!!!!!!!!!!premiere erreur");
+    handleDisconnect();
+  }
   con.query("SELECT * FROM employe where num_service = 30", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
+    if (err) 
+    {
+      console.log("!!!!!!!!!!!!!!deuxième erreur");
+      handleDisconnect();
+    }
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!premier accès");
   });
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   con.connect(function(err) {
-    if (err) throw err;
+    if (err) 
+    {
+      console.log("!!!!!!!!!!!!!!troisième erreur");
+      handleDisconnect();
+    }
     con.query("SELECT * FROM employe where num_service = 30", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
+      if (err) 
+      {
+        console.log("!!!!!!!!!!!!!!quatrième erreur");
+        handleDisconnect();
+      }
+      console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!second accès");
     });
   });
 
