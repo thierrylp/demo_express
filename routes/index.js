56f5e7eedac7a19f8e841c402e3c76e0bcada0fe
tlp <thierrylepors@gmail.com>
2019-07-15 14:50:01 +0200

    test 2  mysql

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-/*var con = mysql.createConnection({
+var con = mysql.createConnection({
   host: "btynqm6xaoikm4l8eix4-mysql.services.clever-cloud.com",
   user: "uulhz2pqtzq43cz5",
   password: "TtttrS8HsgbIROoJblTM",
   database: "btynqm6xaoikm4l8eix4"
-});*/
-
+});
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM employe where num_service = 30", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
